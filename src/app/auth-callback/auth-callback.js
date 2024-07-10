45c7571610520f090f4822fc39235c8a7055b1d4
yoon <miracle7532@naver.com>
2024-07-10 18:38:33 +0900

    Resolve merge conflicts

---
+++ src/app/auth-callback/auth-callback.js
@@ -0,0 +1,26 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import supabase from '../api/supabaseaApi'; // 실제 경로로 수정하세요
+
+export default function AuthCallback() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleAuthCallback = async () => {
+      const { data, error } = await supabase.auth.getSessionFromUrl({
+        storeSession: true,
+      });
+
+      if (error) {
+        console.error('Error handling auth callback:', error);
+      } else {
+        router.push('/'); // 로그인 후 리디렉션할 경로
+      }
+    };
+
+    handleAuthCallback();
+  }, [router]);
+
+  return <div>Authenticating...</div>;
+}
