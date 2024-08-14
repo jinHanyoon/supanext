3c85ffa729c796a84e844fbe5ddfd4736da8da58
yoon <miracle7532@naver.com>
2024-08-14 17:43:58 +0900

    video

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import Header from "./component/header/header";
@@ -8,11 +10,16 @@ import Sidebar from "./component/sidebar/page";
 import css from "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
 
-};
 
 export default function RootLayout({ children }) {
+
+  const [videoPlaying, setVideoPlaying] = useState(false);
+
+  const handleConfirm = () => {
+    setVideoPlaying(true);}
+
+  
   return (
     <html lang="en">
     <head>
@@ -28,9 +35,26 @@ export default function RootLayout({ children }) {
 
 
         <body>
-        <video autoPlay loop muted className="fixed top-0 left-0 w-full h-full object-cover -z-10">
-    <source src="/img/bg.webm" type="video/mp4" />
-    </video>    
+        {!videoPlaying && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50">
+            <button
+              onClick={handleConfirm}
+              className="bg-white text-black p-2 rounded"
+            >
+              확인
+            </button>
+          </div>
+        )}
+
+
+{videoPlaying && (
+          <video autoPlay loop muted className="fixed inset-0 object-cover -z-10">
+            <source src="/img/bg.webm" type="video/mp4" />
+          </video>
+        )}
+
+ 
+
       <Header/>
 
 
