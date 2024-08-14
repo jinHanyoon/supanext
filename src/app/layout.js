ae9ebbd3d2c2aaf91670a12858233ac62a0ecc20
yoon <miracle7532@naver.com>
2024-08-14 18:19:26 +0900

    good

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,13 +36,12 @@ export default function RootLayout({ children }) {
 
         <body>
         {!videoPlaying && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50">
-            <button
-              onClick={handleConfirm}
-              className="bg-white text-black p-2 rounded"
-            >
-              확인
-            </button>
+          <div className="fixed top-0 left-0 inset-0 flex items-center justify-center bg-black bg-opacity-90 z-50 w-full h-full">
+<button onClick={handleConfirm} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+<span className="opacity-80 relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-100 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-black font-bold text-xl	">
+Start Web
+</span>
+</button>
           </div>
         )}
 
