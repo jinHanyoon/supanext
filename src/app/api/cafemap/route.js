716db8680a48d32af0effb0a3ec8e4878d9879cb
yoon <miracle7532@naver.com>
2024-11-04 15:33:28 +0900

    cors

--- src/app/api/cafemap/route.js
+++ src/app/api/cafemap/route.js
@@ -1,23 +1,23 @@
 import { NextResponse } from 'next/server';
 
-
 export async function GET() {
     try {
         const response = await fetch('http://api.kcisa.kr/openapi/API_CIA_090/request?serviceKey=' + process.env.CAFEMAP_API_KEY);
-        const xmlData = await response.text();
+        const textData = await response.text();
         
-        // XML 데이터를 그대로 전달
-        return new NextResponse(xmlData, {
+        return new NextResponse(JSON.stringify({ data: textData }), {
             status: 200,
             headers: {
-                'Content-Type': 'text/xml', // XML 컨텐츠 타입으로 변경
+                'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             },
         });
+        
     } catch (error) {
         console.error('Error:', error);
-        return new NextResponse(JSON.stringify({ error: '데이터 가져오기 실패' }), {
+        return new NextResponse(JSON.stringify({ error: error.message }), {
             status: 500,
             headers: {
                 'Content-Type': 'application/json',
