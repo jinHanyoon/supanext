1aa091f561014663e197f8cf279742856ea36d0f
yoon <miracle7532@naver.com>
2024-11-04 13:55:49 +0900

    cors 설정

--- src/app/api/cafemap/route.js
+++ src/app/api/cafemap/route.js
@@ -7,10 +7,18 @@ export async function GET() {
         const response = await fetch('http://api.kcisa.kr/openapi/API_CIA_090/request?serviceKey=' + process.env.CAFEMAP_API_KEY  );
         const textData = await response.text();
         
+
         // 받아온 데이터를 콘솔에 출력
         // console.log('API 응답 데이터:', textData);
-        
-        return NextResponse.json({data:textData});
+            // CORS 헤더 추가
+    const res = NextResponse.json({ data: textData });
+    res.headers.append('Access-Control-Allow-Origin', '*'); // 모든 도메인 허용
+    res.headers.append('Access-Control-Allow-Methods', 'GET, OPTIONS'); // 허용할 HTTP 메서드
+    
+    return res;
+
+
+        // return NextResponse.json({data:textData});
         
     } catch (error) {
         console.error('Error:', error);
@@ -18,3 +26,5 @@ export async function GET() {
     }
 
 }
+
+
