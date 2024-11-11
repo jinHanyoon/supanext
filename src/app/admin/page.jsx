79ab3ba20a7a0b4b98acd5a12b072fff1bc5db58
yoon <miracle7532@naver.com>
2024-11-11 12:26:28 +0900

    어드민 페이지 수정

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -7,24 +7,21 @@ import { useRouter } from 'next/navigation';
 export default function Admin() {
   const vip01 = '87136bc1-763b-4aae-b250-10f214d3c885';
   const vip02 = 'dab01cd0-a04a-4115-8932-dfad15d36fc7';
-  const { userUUID } = useUserSession();
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
- useEffect(()=>{
-  if (userUUID === vip01 || userUUID === vip02) {
-    setIsLoading(true);
-    console.log(`VIP 사용자입니다: ${userUUID}`);
-    // VIP 사용자에 대한 추가 작업이 필요하다면 여기에 작성
-  }
+//  useEffect(()=>{
+//   if (userUUID === vip01 || userUUID === vip02) {
+//     setIsLoading(true);
+//     console.log(`VIP 사용자입니다: ${userUUID}`);
 
-},[userUUID,vip02,vip01])
+//   }
+
+// },[userUUID,vip02,vip01])
 
   return (
     <div >
-    {isLoading &&(
       <List />
-      )}
     </div>
   );
 }
